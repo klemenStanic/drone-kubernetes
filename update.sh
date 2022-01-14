ae34cefa6920255146fc916904cdba4a08b4483f
klemenStanic <stanic.klemen@gmail.com>
2022-01-14 11:27:14 +0100

    a

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+printenv
 envsubst < /tmp/kube_config > kube_config
 KUBECONFIG=kube_config
 export KUBECONFIG
 
 kubectl config use-context --kubeconfig=kube_config $PLUGIN_K8S_CLUSTER_NAME 
-kubectl rollout restart deployment $PLUGIN_K8S_DEPLOYMENT_NAME -n develop
+kubectl rollout restart deployment $PLUGIN_K8S_DEPLOYMENT_NAME -n $PLUGIN_K8S_NAMESPACE
 echo "Redeployed $PLUGIN_K8S_DEPLOYMENT_NAME to K8S cluster at $PLUGIN_K8S_SERVER"
